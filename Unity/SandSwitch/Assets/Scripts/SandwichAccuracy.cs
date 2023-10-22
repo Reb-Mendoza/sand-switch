86aac4c2d6839e6c25b8bbdc90c219f7bf11995e
Reb Mendoza <rebisaguava@gmail.com>
2023-10-22 14:40:55 -0400

    8/22

--- Unity/SandSwitch/Assets/Scripts/SandwichAccuracy.cs
+++ Unity/SandSwitch/Assets/Scripts/SandwichAccuracy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SandwichAccuracy : MonoBehaviour
 {
+    public float sqrLenCenters;
     public float lenOffset;
     public float lenTargetComponent;
     public int score = 0;
@@ -14,24 +15,23 @@ public class SandwichAccuracy : MonoBehaviour
             //Have them stick together.
             gameObject.AddComponent<FixedJoint>().connectedBody = collisionInfo.collider.gameObject.GetComponent<Rigidbody>();
             //Measure how accurately placed they are by measuring the distance between the component's centers.
-            lenOffset = (gameObject.transform.position - collisionInfo.collider.gameObject.transform.position).magnitude;
+            //lenOffset uses the Pythagoren Theorm to find the relative horizontal offset of the ingredients given the distance between their centers as well as the thickness of each ingredient.
+            sqrLenCenters = (gameObject.transform.position - collisionInfo.collider.gameObject.transform.position).sqrMagnitude;
+            lenOffset = Mathf.Sqrt(sqrLenCenters - (((gameObject.transform.lossyScale.y / 2) + (collisionInfo.collider.gameObject.transform.lossyScale.y / 2)) * ((gameObject.transform.lossyScale.y / 2) + (collisionInfo.collider.gameObject.transform.lossyScale.y / 2))));
             lenTargetComponent = collisionInfo.collider.gameObject.transform.lossyScale.x;
             //lenOffset / lenTargetComponent has possible values of 0-2. Closer to 0 is better. 1 means the sandwich component is exactly halfway to the center.
             //Judge the accuracy of the ingredient on a scale of 1-3.
+            UnityEngine.Debug.Log(Vector3.ProjectOnPlane(new Vector3(4f,3f,3f), new Vector3(0f,1f,0f)).magnitude);
+            UnityEngine.Debug.DrawRay(Vector3.zero,(Vector3.ProjectOnPlane(new Vector3(4f,3f,3f), new Vector3(0f,1f,0f))),Color.black,100,false);
             if ((lenOffset / lenTargetComponent) < 1f){
                 score++;
-                UnityEngine.Debug.Log("One Star");
             }
             if ((lenOffset / lenTargetComponent) < 0.5f){
                 score++;
-                UnityEngine.Debug.Log("Two Star");
             }
-            if ((lenOffset / lenTargetComponent) < 0.15f){
+            if ((lenOffset / lenTargetComponent) < 0.1f){
                 score++;
-                UnityEngine.Debug.Log("Three Star");
             }
-            //IMPORTANT - Need to make the thickness of the component not matter in the calculation!!!
-            
         }
     }
 }
