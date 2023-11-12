e4c5fb6a3ddc06176792f0bc44496b2fadbcc2dc
Reb Mendoza <rebisaguava@gmail.com>
2023-11-12 13:21:00 -0500

    shesh

--- Unity/SandSwitch/Assets/Scripts/MissileInstance.cs
+++ Unity/SandSwitch/Assets/Scripts/MissileInstance.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class MissileInstance : MonoBehaviour
 {
-    public float damageValue = 0;
+    public float damageValue = 0f;
 
     void FixedUpdate(){
         //Move the missile forward according to the direction it's facing.
-        gameObject.transform.position += Vector3.forward * 0.1f;
+        transform.Translate(Vector3.forward * 0.1f);
     }
 
     void OnTriggerEnter(Collider other){
         //If the missile hits a player, disappear and deal damage.
         UnityEngine.Debug.Log("Collision!");
         if (other.tag == "Player"){
-            other.gameObject.GetComponent<PlayerManager>().AdjustHitpoints(damageValue);
+            other.gameObject.GetComponentInParent<PlayerManager>().AdjustHitpoints(-1 * damageValue);
             UnityEngine.Debug.Log("You've been hit by a missile for " + damageValue + " damage!");
         }
     }
--- Unity/SandSwitch/Assets/Scripts/ObstacleManager.cs
+++ Unity/SandSwitch/Assets/Scripts/ObstacleManager.cs
@@ -8,19 +8,37 @@ public class ObstacleManager : MonoBehaviour
     public Vector3 targetPosition;
     public Vector2 randomRadius;
     public int shoot;
-    public void Start(){
-        missile = GameObject.FindWithTag("Missile");
-    }
     public void SpawnAimedMissile(float damage){
         //Spawn a missile facing the player.
         targetPosition = GameObject.FindWithTag("Player").transform.position;
-        randomRadius = UnityEngine.Random.insideUnitCircle * 20;
-        Instantiate(missile, (new Vector3(randomRadius.x, 0, randomRadius.y) + targetPosition), Quaternion.LookRotation(targetPosition - (new Vector3(randomRadius.x, 0, randomRadius.y) + targetPosition))).GetComponent<MissileInstance>().damageValue = damage;
+        randomRadius = UnityEngine.Random.insideUnitCircle.normalized * 20;
+        Instantiate(missile, (new Vector3(randomRadius.x, 0, randomRadius.y) + targetPosition), (Quaternion.LookRotation(targetPosition - (new Vector3(randomRadius.x, 0, randomRadius.y) + targetPosition)) * Quaternion.Euler(0,RandomGaussian(-50,50),0))).GetComponent<MissileInstance>().damageValue = damage;
     }
     public void FixedUpdate(){
-        if (shoot > 1){
+        if (shoot > 0){
             SpawnAimedMissile(20);
             shoot--;
         }
+    }
+    public static float RandomGaussian(float minValue = 0.0f, float maxValue = 1.0f)
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * UnityEngine.Random.value - 1.0f;
+            v = 2.0f * UnityEngine.Random.value - 1.0f;
+            S = u * u + v * v;
+        }
+        while (S >= 1.0f);
+
+        // Standard Normal Distribution
+        float std = u * Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+
+        // Normal Distribution centered between the min and max value
+        // and clamped following the "three-sigma rule"
+        float mean = (minValue + maxValue) / 2.0f;
+        float sigma = (maxValue - mean) / 3.0f;
+        return Mathf.Clamp(std * sigma + mean, minValue, maxValue);
     }
 }
